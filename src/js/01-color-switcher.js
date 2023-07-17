219ba540652d9b3c70e5a34c07099a4056e7c04f
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-17 23:09:39 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const bodyChangeColor = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  let intervalId = null;
+
+  btnStop.setAttribute('disabled', '');
+
+  btnStart.addEventListener('click', el => {
+    el.target.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+
+    intervalId = setInterval(() => {
+        bodyChangeColor.style.backgroundColor = getRandomHexColor();}, 1000);
+        console.log('start');
+    });
+
+    btnStop.addEventListener('click', el => {
+        el.target.setAttribute('disabled', true);
+        btnStart.removeAttribute('disabled');
+
+        clearInterval(intervalId);
+        console.log('stop');
+    })
 
