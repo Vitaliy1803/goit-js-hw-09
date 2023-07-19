e7084b578ed7a5355abd4ff6c5d68f8b869343cd
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-19 19:53:15 +0300

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix, { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+let firstDaleyInput = null;
+let daleyStepInput = null;
+let amountInput = null;
+
+
+const onSubmitForm = event => {
+  event.preventDefault();
+
+  if(!event.target.tagName === 'BUTON') {
+    return;
   }
+
+  const {
+    elements: {delay, step, amount},
+  } = event.currentTarget;
+
+  firstDaleyInput = Number(delay.value);
+  daleyStepInput = Number(step.value);
+  amountInput = Number(amount.value);
+  
 }
+
+
+
+
+function createPromise(position, delay) {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve({position, delay});
+  } else {
+    reject({position, delay});
+  }
+    }, delay);
+  })
+  
+}
+
+
+form.addEventListener('submit', onSubmitForm);
