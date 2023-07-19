e51974f624b9ae19773bead6f4f0ab2b7ee87503
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-19 20:14:46 +0300

    promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,8 +5,21 @@ let firstDaleyInput = null;
 let daleyStepInput = null;
 let amountInput = null;
 
+function createPromise(position, delay) {
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve({position, delay});
+  } else {
+    reject({position, delay});
+  }
+    }, delay);
+  });
+  
+}
 
-const onSubmitForm = event => {
+const onSubmitForm = (event) => {
   event.preventDefault();
 
   if(!event.target.tagName === 'BUTON') {
@@ -20,25 +33,21 @@ const onSubmitForm = event => {
   firstDaleyInput = Number(delay.value);
   daleyStepInput = Number(step.value);
   amountInput = Number(amount.value);
-  
-}
 
+  for (let i = 1; i < amountInput; i++) {
+    createPromise(i, firstDaleyInput)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 
+     firstDaleyInput += daleyStepInput; 
+    }
 
+    event.currentTarget.reset();
 
-function createPromise(position, delay) {
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve({position, delay});
-  } else {
-    reject({position, delay});
-  }
-    }, delay);
-  })
-  
-}
-
+};
 
 form.addEventListener('submit', onSubmitForm);
